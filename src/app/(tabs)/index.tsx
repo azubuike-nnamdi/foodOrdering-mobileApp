b22704b216930f99499ff19d809151d10fd294f0
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-04-11 00:06:57 +0100

    chore: redirect index page :hammer:

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,21 +1,5 @@
-import { View } from '@/src/components/Themed';
-import products from '@/assets/data/products';
-import { ProductListItem } from '@/src/components/ProductListItem';
-import { FlatList } from 'react-native';
+import { Redirect } from "expo-router";
 
-//flatlist helps render infinite scrollable lists
-//flatlist has 2 required properties: data and rendereItem
-//render item should be a function that tells flatlist how each item should be rendered
-export default function MenuScreen() {
-  return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-      numColumns={2}
-      contentContainerStyle={{ gap: 10, padding: 10 }}
-      columnWrapperStyle={{ gap: 10 }}
-    />
-  );
+export default function TabIndex() {
+  return <Redirect href={'/menu/'} />;
 }
-
-
