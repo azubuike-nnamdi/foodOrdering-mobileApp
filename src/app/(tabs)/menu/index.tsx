f1bbc135df69512d31b0e1e3ad56b01b97d77613
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-04-11 00:07:54 +0100

    feat: menu page  and route :sprinkles:

---
+++ src/app/(tabs)/menu/index.tsx
@@ -0,0 +1,21 @@
+import { View } from '@/src/components/Themed';
+import products from '@/assets/data/products';
+import { ProductListItem } from '@/src/components/ProductListItem';
+import { FlatList } from 'react-native';
+
+//flatlist helps render infinite scrollable lists
+//flatlist has 2 required properties: data and rendereItem
+//render item should be a function that tells flatlist how each item should be rendered
+export default function MenuScreen() {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
+  );
+}
+
+
